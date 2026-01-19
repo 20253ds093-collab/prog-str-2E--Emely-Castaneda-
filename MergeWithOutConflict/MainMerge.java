b403bf0520b532e104cd674a3b857ba3e1f0465e
dxemely <20253ds093@utez.edu.mx>
2026-01-18 22:48:52 -0600

    chore: base actividad git merge withoutconflict

--- MergeWithOutConflict/MainMerge.java
+++ MergeWithOutConflict/MainMerge.java
@@ -1,8 +1,7 @@
-public class Main {
+public class MainMerge {
     public static void main(String[] args) {
         System.out.println("Semana 2");
         System.out.println("Versión base");
-        System.out.println("2 + 2 = 4");
-        System.out.println("5 * 3 = 15");
+
     }
 }
